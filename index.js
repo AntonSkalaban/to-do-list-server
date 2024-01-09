3ffae62ec27e2a0895fe500673b54fd56b0a8bb7
Anton Skalaban <antonskalaban@MacBook-Air-Anton.local>
2024-01-09 19:02:52 +0300

    feat: connect DB

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
 import 'dotenv/config';
 
-
 const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
 
 const app = express();
 
 app.use(express.json());
-
 app.use(
   cors({
     origin: '*',
@@ -19,6 +19,7 @@ app.use(
 
 const startApp = async () => {
     try {
+      await mongoose.connect(DB_URL);
       app.listen(PORT, () => console.log('server started on port ' + PORT));
     } catch (e) {
       console.log(e);
